0829ad33463e6659a58b233cb26b3c991211fe00
KhanAli <khan.ali.baku@gmail.com>
2022-09-28 21:02:49 -0700

    Negative test invalid credentials

--- cypress/e2e/auth.cy.js
+++ cypress/e2e/auth.cy.js
@@ -1,17 +1,33 @@
 describe('Authentification', () => {
-    it('Sign in with valid credentials', () => {
+    it.skip('Sign in with valid credentials', () => {
         cy.visit('/user/login')
 
         cy.get('#normal_login_email')
             .type('orkhanmaharramov2019@gmail.com')
         cy.get('#normal_login_password')
-            .type('MajeraDoluAmerica!@2022')
+            .type('MajeraDoluAmerica!@')
         cy.get('.login-form-button')
             .click()
 
         cy.get('.ant-avatar-square')
             .should('be.visible')
         cy.location('pathname')
-            .should('include','profile')
+            .should('include', 'profile')
     });
+
+
+    it('Sign in with invalid credentials', () => {
+        cy.visit('/user/login')
+
+        cy.get('#normal_login_email')
+            .type('orkhanmaharramov2019@gmail.com')
+        cy.get('#normal_login_password')
+            .type('MajeraDoluAmerica!@')
+        cy.get('.login-form-button')
+            .click()
+
+        cy.get('.ant-notification-notice-message')
+            .should('have.text', 'Auth failed')
+
+    })
 })
